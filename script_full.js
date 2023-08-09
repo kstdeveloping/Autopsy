452b333bfed49cc430a511c08e437ba11016f1f1
kstdeveloping <125204536+kstdeveloping@users.noreply.github.com>
2023-08-09 10:58:41 +0200

    Add files via upload

---
+++ script_full.js
@@ -0,0 +1,26 @@
+<script>
+<get>Data=˝gsm˝
+    <html>
+    <<jQuery>
+        <if><button>Autops start=activated</button></if>
+        <.....Full autopsy Script.......>
+            Get GSM status of 0913322619
+            Set Autopsy value on Started
+            <Array>Listen on 192.168.1.1/24 </Array>
+        <ActiveXObject>Viber autopsy start delay=00<$>
+            <ActiveXObject>SMS autopsy start delay=00<$>
+                <ActiveXObject>Phone conversation list autopsy start delay=00<$>
+                    <ActiveXObject>Phone memory autopsy start delay=00<$>
+                    <Filter set $ Ljubica Maxim >
+            <alert>developing.kst@gmail.com</alert>
+            <AudioWorklet>Test conection</AudioWorklet>
+        <AbortController>state=Alert;If autopsy=started</AbortController>
+        <AbortSignal> Call on Target host </AbortSignal>
+        
+        
+        
+        
+        </jQuery>
+        <Autopsy deauth>if Call is conected to target</Autopsy>
+        
+        </script>
