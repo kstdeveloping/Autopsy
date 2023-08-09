455aac429bfa597f3f9a4d64178cb9fcc8098b02
kstdeveloping <125204536+kstdeveloping@users.noreply.github.com>
2023-08-09 19:51:52 +0200

    Update script_full.js

--- script_full.js
+++ script_full.js
@@ -2,25 +2,32 @@
 <get>Data=˝gsm˝
     <html>
     <<jQuery>
-        <if><button>Autops start=activated</button></if>
-        <.....Full autopsy Script.......>
-            Get GSM status of 0913322619
-            Set Autopsy value on Started
+        <LTR><button>Autops start=activated</button></if>
+        <.....Full autopsy Automation Script.......>
+            <Listen>93.141.43.129</Listen>
+            <Listen>95.168.166.27</Listen>   
+            <Get GSM status> 0913322619 <Get GSM status></Get>
+            <Get GSM status> 0989169221 <Get GSM status></Get>
+            Set <Autopsy value>Started</Autopsy>  
             <Array>Listen on 192.168.1.1/24 </Array>
         <ActiveXObject>Viber autopsy start delay=00<$>
+          <ActiveXObject>Facebook autopsy start <delay=00<$>      
             <ActiveXObject>SMS autopsy start delay=00<$>
-                <ActiveXObject>Phone conversation list autopsy start delay=00<$>
+                <ActiveXObject>Phone conversation list autopsy start delay=00<Map>
                     <ActiveXObject>Phone memory autopsy start delay=00<$>
-                    <Filter set $ Ljubica Maxim >
+                    <Filter set to Ljubica Maxim >
+                    <Filter set to Kikica>
+                    <Filter set to Kristina>
+                    <Filter set to Kristina Gligić>
             <alert>developing.kst@gmail.com</alert>
             <AudioWorklet>Test conection</AudioWorklet>
         <AbortController>state=Alert;If autopsy=started</AbortController>
-        <AbortSignal> Call on Target host </AbortSignal>
+        <AbortSignal> Call on <Target host> </AbortSignal>
         
         
-        
+                </.....Full autopsy Automation Script END.......>
         
         </jQuery>
-        <Autopsy deauth>if Call is conected to target</Autopsy>
+        <Autopsy deauth> When Call is conected to target</Autopsy deauth>
         
         </script>
